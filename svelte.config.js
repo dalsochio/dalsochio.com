284a1cae173d8038cf08b225686f66882ee3c590
Thiago Dalsochio <dalsochiothiago@gmail.com>
2022-10-18 17:56:51 +0000

    SCSS Add.

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,17 @@
+import preprocess from "svelte-preprocess";
 import firebase from "svelte-adapter-firebase";
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
   kit: {
-   adapter: firebase(),
+    adapter: firebase(),
   },
+
+  preprocess: [
+    preprocess({
+      scss: {
+        prependData: '@use "src/variables.scss" as *;',
+      },
+    }),
+  ],
 };
